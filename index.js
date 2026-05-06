a542fc06f247d541f1fdf3200737da1f42e885af
Badsha-Nishan <badshanisan14@gmail.com>
2026-05-06 18:33:30 +0600

    express js server data create with nodemon

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 
+const users = [
+  { id: 1, name: "Badsha Nishan", email: "khau@dau.com" },
+  { id: 2, name: "Shornali Akter", email: "shornali@bornali.com" },
+  { id: 3, name: "Aramer Ghum", email: "aram@ghum.com" },
+  { id: 4, name: "Petuk Jamai", email: "petuk@jamai.com" },
+];
+
 app.get("/", (req, res) => {
   res.send("Hello from users world!");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.get("/products", (req, res) => {
+  res.send("Products are jumping up!");
 });
 
 app.listen(port, () => {
